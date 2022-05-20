a87837e0b544d72c8999581cc628d2040de7c3be
ambekarsameer <ambekarsameer@gmail.com>
2022-05-20 20:13:40 +0200

    pushing json reader

--- json_reader.py
+++ json_reader.py
@@ -1,30 +1,36 @@
 import os 
 import json
 
-#open a text file writer 
-textf = open('output.txt', 'w')
+import re
 
 #read json file from json_files directory 
-
-json_dir = 'json_files\metavaljson'
+#open a text file writer 
+textf = open('output.txt', 'w')
+json_dir = 'meta16'
 data = {}
 textf.write('Filename || Accuracy"\n')
 for f1 in os.listdir(json_dir):
     #print(f1)
     d1 ={}
-    f2 = os.path.join(json_dir, f1)
-    with open(f2, 'r') as f:
-        d1 = json.load(f)
-        #print(data)
-    f1 = f1.split('.json')[0]
-    data[f1] = d1
-    #writer 
-    text1 = f1
-    text2 = str(d1['LAS']['aligned_accuracy'])
+    print(f1)
+    if f1.endswith('.json'):
+        lang_name = f1.split('-')[0]
+        f2 = os.path.join(json_dir, f1)
+        with open(f2, 'r',  encoding="utf8") as f:
+            d1 = json.load(f)
+            #print(data)
+        f1 = f1.split('.json')[0]
+        data[f1] = d1
+        #writer 
 
-    #write to text file 
+        text1 = f1
+        text2 = str(d1['LAS']['aligned_accuracy'])
+
+        #write to text file 
+        dig = re.findall(r'\d+', f1) 
+        print(int(dig[0]))
+        textf.write(text1 + '\t'+ '||' + text2 + '\n')
     
-    textf.write(text1 + '\t'+ '||' + text2 + '\n')
 
 textf.close()
     
